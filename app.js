4a884fa2acea8673428a22642699ae4e130daabd
Maya Venegas <venegama@oregonstate.edu>
2025-11-03 22:04:05 -0800

    A lot tbh

--- app.js
+++ app.js
@@ -1,45 +1,44 @@
-const start = document.getElementById("start");
-const stop = document.getElementById("stop");
-const reset = document.getElementById("reset");
-const timer = document.getElementById("timer");
+//Add task user input
 
-let timeLeft = 1500; //# of secs in 25 mins
-let interval;
+var allWords = [];
 
-const updateTimer = () => {
-    const minutes = Math.floor(timeLeft / 60);
-    const seconds = timeLeft % 60;
+var taskTitleInput = document.getElementById("new-task-title");
+const addTaskButton = document.getElementById("in-progress");
 
-    timer.innerHTML = 
-    `${minutes.toString().padStart(2, "0")}
-    :
-    ${seconds.toString().padStart(2, "0")}`;
-};
+/*taskTitleInput.addEventListener("change", addTask)*/
 
-const startTimer = () => {
-    interval = setInterval(() => {
-        timeLeft--;
-        updateTimer();
+addTaskButton.addEventListener("click", addTask);
 
-        if(timeLeft === 0){
-            clearInterval(interval);
-            alert("Time's up!");
-            timeLeft = 1500;
-            updateTimer();
-        }
-    }, 1000);
+function addTask(event) {
+    console.log("Add task happened: ", event);
+
+    //Create a checkbox
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    //Checkbox event listener
+    checkbox.addEventListener("click", function () {
+        taskDiv.classList.toggle("completed");
+    })
+
+    // Get current input value
+    var userInput = taskTitleInput.value;
+
+    // Save to the array
+    allWords.push(userInput);
+
+    // Create a new task div    
+    var taskDiv = document.createElement("div");
+    taskDiv.classList.add("in-progress-task");
+
+    // Add the user input and checkbox as the task content
+    var taskText = document.createElement("span");
+    taskText.textContent = userInput;
+
+    taskDiv.appendChild(checkbox);
+    taskDiv.appendChild(taskText);
+
+    // Add task div to the container
+    var container = addTaskButton.parentNode;
+    container.appendChild(taskDiv);
 }
-
-const stopTimer = () => {
-    clearInterval(interval);
-}
-
-const resetTimer = () => {
-    clearInterval(interval);
-    timeLeft = 1500;
-    updateTimer();
-}
-
-start.addEventListener("click", startTimer);
-stop.addEventListener("click", stopTimer);
-reset.addEventListener("click", resetTimer);
