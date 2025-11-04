31f9973c1512c0f1cf8ca38e8eefe6a88e1227a3
Maya Venegas <venegama@oregonstate.edu>
2025-11-03 23:02:20 -0800

    Added checkbox and black title modal

--- app.js
+++ app.js
@@ -5,12 +5,42 @@ var allWords = [];
 var taskTitleInput = document.getElementById("new-task-title");
 const addTaskButton = document.getElementById("in-progress");
 
+// Get modal elements
+const modal = document.getElementById("blank-task-modal");
+const cancelModal = document.getElementById("cancel-modal");
+const continueModal = document.getElementById("continue-modal");
+
 /*taskTitleInput.addEventListener("change", addTask)*/
 
-addTaskButton.addEventListener("click", addTask);
+addTaskButton.addEventListener("click", function () {
+const userInput = taskTitleInput.value.trim();
 
-function addTask(event) {
-    console.log("Add task happened: ", event);
+if(userInput === "") {
+    modal.style.display = "block";
+} else {
+    createTask(userInput);
+}
+});
+
+cancelModal.addEventListener("click", function () {
+    modal.style.display = "none";
+});
+
+continueModal.addEventListener("click", function () {
+    const userInput = taskTitleInput.value.trim();
+    createTask(userInput);
+    modal.style.display = "none";
+});
+
+
+function createTask(userInput) {
+
+    // Save to the array
+    allWords.push(userInput);
+
+    // Create a new task div    
+    var taskDiv = document.createElement("div");
+    taskDiv.classList.add("in-progress-task");
 
     //Create a checkbox
     var checkbox = document.createElement("input");
@@ -20,16 +50,6 @@ function addTask(event) {
     checkbox.addEventListener("click", function () {
         taskDiv.classList.toggle("completed");
     })
-
-    // Get current input value
-    var userInput = taskTitleInput.value;
-
-    // Save to the array
-    allWords.push(userInput);
-
-    // Create a new task div    
-    var taskDiv = document.createElement("div");
-    taskDiv.classList.add("in-progress-task");
 
     // Add the user input and checkbox as the task content
     var taskText = document.createElement("span");
